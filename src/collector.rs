d95624bb4caa2b3efc13a64469ac459d3e5350a0
Frumple <frumple1@gmail.com>
2024-03-22 07:20:07 -0700

    Remove warning comment after updating to Rust 1.77.0.

--- src/collector.rs
+++ src/collector.rs
@@ -5,9 +5,6 @@ pub mod util;
 use url::Url;
 
 pub trait HttpServer {
-  // This opaque-hidden-inferred-bound warning should not be fired.
-  // It has been fixed in https://github.com/rust-lang/rust/issues/113538
-  // TODO: Remove this comment when it has been fixed.
   async fn new() -> Self;
   fn base_url(&self) -> Url;
 }
