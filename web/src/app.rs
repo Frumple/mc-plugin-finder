a166c2b8e3e4c3ac29b84a0450ac7667fcea1677
Frumple <frumple1@gmail.com>
2024-12-12 16:16:25 -0800

    Fix premium icon.

--- web/src/app.rs
+++ web/src/app.rs
@@ -13,6 +13,7 @@ use time::{OffsetDateTime, format_description};
 use mc_plugin_finder::database::common::search_result::{SearchParams, SearchParamsSort, SearchResult, SearchResultSpigot, SearchResultModrinth, SearchResultHangar};
 
 const NO_ICON_IMAGE_URL: &str = "images/no-icon.svg";
+const PREMIUM_IMAGE_URL: &str = "images/premium.svg";
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Params)]
 pub struct WebSearchParams {
@@ -867,11 +868,7 @@ fn SpigotResourceInner(
             <div class="search-row__text-contents">
                 <div class="search-row__cell-title">
                     <Show when=move || { is_spigot_premium }>
-                        <span class="search-row__plugin-premium">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#000000" style="vertical-align: bottom;">
-                                <path d="M446-216h67v-47q49-8 81-42t32-79q0-45-27.5-77T514-514q-61-22-80.5-37.5T414-592q0-20 17.5-33t45.5-13q28 0 49 13.5t28 36.5l59-25q-12-33-38.5-55.5T513-697v-47h-66v48q-45 10-72 38.5T348-591q0 45 30.5 76.5T475-460q45 16 65.5 34t20.5 42q0 26-21 43.5T488-323q-33 0-58.5-22T395-402l-62 26q12 42 42 71.5t71 40.5v48Zm34 120q-79 0-149-30t-122.5-82.5Q156-261 126-331T96-480q0-80 30-149.5t82.5-122Q261-804 331-834t149-30q80 0 149.5 30t122 82.5Q804-699 834-629.5T864-480q0 79-30 149t-82.5 122.5Q699-156 629.5-126T480-96Zm0-72q130 0 221-91t91-221q0-130-91-221t-221-91q-130 0-221 91t-91 221q0 130 91 221t221 91Zm0-312Z"/>
-                            </svg>
-                        </span>
+                        <img class="search-row__plugin-premium" src=PREMIUM_IMAGE_URL title="Premium Plugin (requires Spigot login)" alt="Dollar sign icon indicating this plugin is premium on Spigot" loading="lazy" />
                     </Show>
                     <h3 class="search-row__plugin-name">{spigot_name.clone()}</h3>
                     <span>"  "</span>
